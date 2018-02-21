5719d7e6f8b013643a10b51dbae7f0db036ec6ac
Jérémie Kalfon <jkobject@gmail.com>
2018-02-21 10:19:09 +0000

    adding new classes for a better designed project to come

---
+++ HomoSet.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+
+class HomoSet(object):
+    """docstring for HomoSet"""
+
+    def __init__(self, arg):
+        super(HomoSet, self).__init__()
+        self.set = pd.dataframe
