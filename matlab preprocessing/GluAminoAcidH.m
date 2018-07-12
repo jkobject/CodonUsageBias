936132620c9c3e2e488fd7b346c14090223a1b22
Jérémie Kalfon <jkobject@gmail.com>
2018-07-12 09:48:54 +0200

    having a thourough look at Yun's processings of Sn location /Avalue

--- matlab preprocessing/GluAminoAcidH.m
+++ matlab preprocessing/GluAminoAcidH.m
@@ -2,22 +2,16 @@ function [NNe,Xe,Y] = GluAminoAcidH(codonSequence)
 
 global cfE ctE;
 
-% disp('A new calculation begins');
-
-codonFrequency=zeros(1,length(codonSequence));
-
 %% naming principle : 'p'--primilinary; 'c'-- codon or cell;'i'--index;'f'--frequency;
 % disp('codon subsequence coding Glu number calculation begins');
 Eia=find(ismember(codonSequence,'GAG'));
 if (Eia~=0)
-    codonFrequency(Eia)=cfE(1,1);
     NumberCodonEia=length(Eia);
-else NumberCodonEia=0;
+else NumberCodonEia=0; 
 end
 
 Eib=find(ismember(codonSequence,'GAA'));
 if (Eib~=0)
-    codonFrequency(Eib)=cfE(1,2);
     NumberCodonEib=length(Eib);
 else NumberCodonEib=0;
 end
@@ -29,7 +23,6 @@ if (Eipi~=0)
     Eig=Eip(Eipi);
     codonSequenceR=(codonSequence)';
     subsequenceE=codonSequenceR((Eig)');
-    Ef=codonFrequency(Eig);
     NNe=length(subsequenceE);
     
     Xe=[NumberCodonEia,NumberCodonEib];
