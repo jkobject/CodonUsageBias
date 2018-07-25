61744ad63ab1921b30342e8c5a8b9f5d9433ee08
Jérémie Kalfon <jkobject@gmail.com>
2018-07-25 14:06:26 +0200

    adding atrributes, debugging plottings, load homomatrices, access to utils values, adding savings and debugs on many functions

--- PyCUB/homology.py
+++ PyCUB/homology.py
@@ -16,7 +16,9 @@ from mpl_toolkits.mplot3d import Axes3D
 import utils
 
 from bokeh.plotting import *
+from bokeh.io import output_file, save, show
 from bokeh.models import *
+from bokeh.layouts import column
 import matplotlib.pyplot as plt
 
 import pdb
@@ -51,7 +53,7 @@ class homology(object):
     clusters = None
     centroids = None
     metrics = {}
-
+    homocode = 'None'
     nans = None
     nanspos = None
     lenmat = None
@@ -77,7 +79,6 @@ class homology(object):
         """
         will intialize an instance of the object and be used for the loading mechanism
         """
-        # TODO: Replace long params by **kwargs (dict for the data)
         data = kwargs.get("data", None)
         if data is not None:
             self.full = np.asarray(data.get("full")) if data.get("full", None) is not None else None
@@ -96,6 +97,7 @@ class homology(object):
             self.reduced = np.asarray(data.get("reduced")) if data.get("reduced", None) is not None else None
             self.reduced_algo = data.get("reduced_algo", None)
             self.KaKs_Scores = data.get("KaKs_Scores", None)
+            self.homocode = data.get("homocode", 'None')
             self.similarity_scores = data.get("similarity_scores", None)
             self.proteinids = data.get("proteinids", [])
             self.isrecent = data.get("isrecent", None)
@@ -112,6 +114,7 @@ class homology(object):
             self.lenmat = kwargs.get("lenmat", None)
             self.names = kwargs.get("names", None)
             self.doub = kwargs.get("doub", None)
+            self.homocode = kwargs.get("homocode", 'None')
             self.doublonpos = kwargs.get("doublonpos", None)
             self.GCcount = kwargs.get("GCcount", None)
             self.reduced = kwargs.get("reduced", None)
@@ -126,12 +129,14 @@ class homology(object):
         """
         removes the list of species from this homology if it exists there
         """
-        tradnames = [utils.speciestable[na] for na in self.names] if type(species[0]) == unicode or \
+        speciestable = dict(utils.speciestable)
+        tradnames = [speciestable[na] for na in self.names] if type(species[0]) == unicode or \
             type(species[0]) == unicode else self.names
         names = list(self.names)
         cluster = []
         mask = np.zeros(len(self.names), dtype=bool)
         self.names = []
+        pdb.set_trace()
         for i, na in enumerate(tradnames):
             if na not in species:
                 self.names.append(names[i])
@@ -151,20 +156,22 @@ class homology(object):
 
     def getdoubpos(self):
         pos = {}
+        speciestable = dict(utils.speciestable)
         if self.doub is not None:
             for i, val in enumerate(self.doub):
                 if val:
                     mean = full[i - 1:i + 1].mean(0)
-                    pos.update({utils.speciestable[self.names[i]]:
+                    pos.update({speciestable[self.names[i]]:
                                 np.divide(np.count_nonzero(mean > self.full, 0), self.nb_unique_species())})
         return pos
 
     def getnanpos(self):
         pos = {}
+        speciestable = dict(utils.speciestable)
         if self.nans is not None:
             for i, val in enumerate(self.nans):
                 if val:
-                    pos.update({utils.speciestable[self.names[i]]:
+                    pos.update({speciestable[self.names[i]]:
                                 np.divide(np.count_nonzero(val > self.full, 0), self.nb_unique_species())})
         return pos
 
@@ -220,12 +227,13 @@ class homology(object):
             red = man.TSNE(n_components=n, perplexity=perplexity).fit_transform(self.full)
         elif alg == 'pca':
             red = PCA(n_components=n).fit_transform(self.full)
-
+        else:
+            raise("wrong algo my friend choose pca or tsne")
         self.reduced = red
         self.reduced_algo = alg
         return red
 
-    def plot(self, reducer="tsne", per=40, interactive=False, D=2, size=40, shownans=True):
+    def plot(self, reducer="tsne", per=40, interactive=False, D=2, size=20, shownans=False):
         """
         plot the dimensionality reduced datapoint of the homology matrix
 
@@ -244,133 +252,110 @@ class homology(object):
         """
         # TODO: use the ancestry distance from homoset (put it in utils)
         # and add the possibility to plot it with colors
+        colormap = list(utils.colormap)
         if self.clusters is not None:
             # colormap = [[rand(256), rand(256), rand(256)] for _ in range(100)]
-            colormap = ["#1abc9c", "#3498db", "#2ecc71", "#9b59b6", '#34495e', '#f1c40f',
-                        '#e67e22', '#e74c3c', '#7f8c8d', '#f39c12']
-            colors = [colormap[x] for x in self.clusters]
+            colors = [colormap[x + 1] for x in self.clusters]
         else:
-            colors = '#1abc9c'
+            colors = [colormap[0]] * len(self.names)
         if shownans:
             for i, val in enumerate(self.nans):
                 if val:
-                    colormap[i] = "#000000"
-        if (self.reduced is None or self.reduced_algo != reducer):
+                    colors[i] = utils.rgb2hex((255 * (val / 18), 255 * (val / 18), 255 * (val / 18)))
+        if self.reduced is None or self.reduced_algo != reducer:
             self.reduce_dim(alg=reducer, n=D, perplexity=per)
-        elif self.reduced.shape[1] == D:
+        elif self.reduced.shape[1] != D:
             self.reduce_dim(alg=reducer, n=D, perplexity=per)
 
         if interactive:
-            # TODO: to test
             print " if you are on a notebook you should write 'from bokeh.io import output_notebook'"
-            if self.clusters is None:
-                colors = [colors] * len(self.names)
-            source = ColumnDataSource(
-                data=dict(x=self.reduced[:, 0], y=self.reduced[:, 1],
-                          label=["species : %s" % utils.speciestable[x__] for x__ in self.names],
-                          doub=self.doub if self.doub is not None else False,
-                          clusters=self.clusters if self.clusters is not None else False,
-                          similarity_scores=self.similarity_scores if
-                          self.similarity_scores is not None else False,
-                          KaKs_Scores=self.KaKs_Scores if self.KaKs_Scores is not None else False,
-                          nans=self.nans if self.nans is not None else False,
-                          color=colors))
+            speciestable = dict(utils.speciestable)
+            data = dict(x=self.reduced[:, 0], y=self.reduced[:, 1],
+                        species=[str(speciestable[n]) for n in self.names],
+                        meanentropy=["%.2f" % self.full[i].mean() for i in range(len(self.names))],
+                        color=colors)
+            if self.doub is not None:
+                data.update({'doub': self.doub})
+            if self.clusters is not None:
+                data.update({'clusters': self.clusters})
+            if self.similarity_scores is not None:
+                data.update({'similarity_scores': self.similarity_scores})
+            if self.KaKs_Scores is not None:
+                data.update({'KaKs_Scores': self.KaKs_Scores})
+            if self.nans is not None:
+                data.update({'nans': self.nans.sum(1)})
+            if self.proteinids is not None:
+                pass  # here use wether or not it is a new protein
+            if self.lenmat is not None:
+                data.update({'lengths': self.lenmat.sum(1)})
+            if self.GCcount is not None:
+                data.update({'gc': self.GCcount})
+            source = ColumnDataSource(data=data)
             output_notebook()
-
+            labe = ["show Cluster", "show Doublon", "show Nans", "show KaKs_Scores",
+                    "show similarity_scores", "show Length"]
+            callback = CustomJS(args=dict(source=source), code=utils.callback)
             radio_button_group = widgets.RadioButtonGroup(
-                labels=["show Cluster", "show Doublon", "show KaKs_Scores",
-                        "show similarity_scores", "Show PhyloDistance"], active=0)
-            callback = CustomJS(args=dict(source=source, colors=colormap), code="""
-            // JavaScript code goes here
-
-            // the model that triggered the callback is cb_obj:
-            var b = cb_obj.value;
-            // models passed as args are automagically available
-            var data = source.data;
-            var len = data.color.length;
-            var col = [];
-            if(b === "show Doublon" && data.doub){
-                var j = 0;
-                for(i=0;i<len;++i){
-                    if(doub[i]){
-                        col[i-1] = colors[j];
-                        col.push(colors[j]);
-                    }else{col.push("#999999");}
-                }
-            }
-            if(b === "show Cluster"){
-                var j = 0;
-                othercol = '#1abc9c'
-                if(data.clusters){
-                    for(i=0;i<len;++i){col.push(colors[data.clusters[i]]);}
-                }else{
-                    for(i=0;i<len;++i){col.push(othercol);}
-                }
-            }
-            if(b === "show KaKs_Scores" && data.KaKs_Scores){
-                var arr = Array(len);
-                while(i--) arr[i] = data.KaKs_Scores[i];
-                arr.sort();
-                var max = arr[len-1];
-                //console.log("max Ka/Ks score here is" + max);
-                for(i=0;i<len;++i){
-                    // normalizing
-                    col.push(rgb(34,256*(data.KaKs_Scores[i]/max),76))
-                }
-            }
-            if(b === "show similarity_scores" && data.similarity_scores){
-                for(i=0;i<len;++i){
-                    col.push(rgb(34,76,256*data.similarity_scores[i]))
-                }
-            }
-            data.color = col;
-            """)
-            radio_button_group.on_click(callback)
-            show(radio_button_group)
-
-            hover = HoverTool(tooltips=[("label", "@label")])
+                labels=labe, callback=callback)
+            hover = HoverTool(tooltips=[("species", "@species"), ("nan_nb", "@nans"),
+                                        ("mean_entr", "@meanentropy"), ("length", "@lengths"), ("GCcount", "@gc")])
             p = figure(title="T-sne of homologous gene X for each species",
-                       tools=[hover, BoxZoomTool(), WheelZoomTool(), SaveTool(), ResetTool()])
+                       tools=[hover, BoxZoomTool(), WheelZoomTool(), SaveTool(), ResetTool()],
+                       plot_width=800, plot_height=600)
             p.circle(x='x', y='y', source=source, color='color', size=10)
+            # TODO: add a plot of the similarity distances
             if self.centroids is not None:
                 p.square(self.centroids[0], self.centroids[0], size=12, color="olive", alpha=0.3)
-            show(p)
-
+            save(column(radio_button_group, p), "utils/templot/homoplot_interactive_" + self.homocode + ".html")
+            show(column(radio_button_group, p))
             print "------------------------------------"
             for key, val in self.metrics.iteritems():
                 print key + ': ' + str(val)
             print "------------------------------------"
-            print self.full.mean(axis=0)
-            print self.full.var(axis=0)
-            print self.KaKs_Scores.mean()
-            print self.similarity_scores.mean()
+            mea = self.full.mean(axis=0)
+            var = self.full.var(axis=0)
+            print "Amino, CUB value mean, variance"
+            for i in range(len(utils.amino)):
+                print utils.amino[i] + ": %.2f" % mea[i] + ", %.3f" % var[i]
+            if self.KaKs_Scores is not None:
+                print "avg KaKs Scores"
+                print self.KaKs_Scores.mean()
+            if self.similarity_scores is not None:
+                print "avg similarity Scores"
+                print self.similarity_scores.mean()
             return p
         else:
             fig = plt.figure(figsize=(40, size))
             if D == 2:
                 ax = fig.add_subplot(111)
-                ax.scatter(self.reduced[:, 0], self.reduced[:, 1], c=colors)
-            if D == 3:
+                ax.scatter(self.reduced[:, 0], self.reduced[:, 1], s=90, c=colors)
+            elif D == 3:
                 ax = Axes3D(fig)
-                ax.scatter(self.reduced[:, 0], self.reduced[:, 1], self.reduced[:, 2], c=colors)
-            if D == 4:
-                # TODO: totest
+                ax.scatter(self.reduced[:, 0], self.reduced[:, 1], self.reduced[:, 2], s=140, c=colors)
+            elif D == 4:
                 ax = fig.add_subplot(111, projection='3d')
                 ax.scatter(self.reduced[:, 0], self.reduced[:, 1],
-                           self.reduced[:, 2], s=self.reduced[:, 3] * 60, c=colors)
+                           self.reduced[:, 2], s=self.reduced[:, 3] * 200, c=colors)
             else:
                 print "please choose a D between 2 and 4"
                 return
             plt.show()
-
+            plt.savefig("utils/templot/homoplot_matplotlib_" + self.homocode + ".pdf")
             print "------------------------------------"
             for key, val in self.metrics.iteritems():
                 print key + ': ' + str(val)
             print "------------------------------------"
-            print self.full.mean(axis=0)
-            print self.full.var(axis=0)
-            print self.KaKs_Scores.mean()
-            print self.similarity_scores.mean()
+            mea = self.full.mean(axis=0)
+            var = self.full.var(axis=0)
+            print "Amino, CUB value mean, variance"
+            for i in range(len(utils.amino)):
+                print utils.amino[i] + ": %.2f" % mea[i] + ", %.3f" % var[i]
+            if self.KaKs_Scores is not None:
+                print "avg KaKs Scores"
+                print self.KaKs_Scores.mean()
+            if self.similarity_scores is not None:
+                print "avg similarity Scores"
+                print self.similarity_scores.mean()
 
     def clusterize_(self, clustering='gaussian', eps=0.8, homogroupnb=None, assess=True):
         """
@@ -410,16 +395,6 @@ class homology(object):
 
             print "Estimated number of clusters using DBscan: " + str(n_clusters_)
         if assess:
-            if utils.phylo_distances is not None:
-                avg_phylodistance = []
-                spe = [utils.speciestable[j] for j in self.names]
-                div = utils.phylo_distances[spe].loc[spe].sum().sum() / (len(spe)**2 - len(spe))
-                for i in range(-1, n_clusters_ - 1):
-                    # Here the first value is for the outliers and the second for the unclusterized
-                    # data points
-                    species = [utils.speciestable[j] for j in self.names[np.argwhere(self.clusters == i)]]
-                    avg_phylodistance.append((utils.phylo_distances[species].
-                                              loc[species].sum().sum() / (len(species)**2 - len(species))) / div)
                 # print 'average phylo distance of the clusters: '+ str(avg_phylodistance)
             try:
                 silhouette = metrics.silhouette_score(self.full, self.clusters).item()
@@ -431,8 +406,21 @@ class homology(object):
             print 'silhouette_score ' + str(silhouette)
             print 'cal_hara ' + str(cal_hara)
             self.metrics.update({'silhouette': silhouette,
-                                 'cal_hara': cal_hara,
-                                 'avg_phylodistance': np.array(avg_phylodistance)})
+                                 'cal_hara': cal_hara})
+            if utils.phylo_distances is not None:
+                speciestable = dict(utils.speciestable)
+                avg_phylodistance = []
+                spe = [speciestable[j] for j in self.names]
+                div = utils.phylo_distances[spe].loc[spe].sum().sum() / (len(spe)**2 - len(spe))
+                for i in range(-1, n_clusters_ - 1):
+                    # Here the first value is for the outliers and the second for the unclusterized
+                    # data points
+                    species = [speciestable[j] for j in self.names[np.argwhere(self.clusters == i)]]
+                    avg_phylodistance.append((utils.phylo_distances[species].
+                                              loc[species].sum().sum() / (len(species)**2 - len(species))) / div)
+                self.metrics.update({'avg_phylodistance': np.array(avg_phylodistance)})
+                print 'avg phylo dist of the clusters: ' + str(avg_phylodistance)
+
         return self.clusters
 
     def _dictify(self):
@@ -444,6 +432,7 @@ class homology(object):
                 "clusters": self.clusters,
                 "full": self.full.tolist() if not (self.full is None) else None,
                 "names": self.names,
+                "homocode": self.homocode,
                 "centroids": self.centroids,
                 "metrics": self.metrics,
                 "KaKs_Scores": self.KaKs_Scores,
